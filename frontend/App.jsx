568f2741ae6a4d6786f8e59bf714b16ba243af09
Khoa100 <minhkhoavu954@gmail.com>
2019-02-07 12:59:49 -0800

    Add files via upload

---
+++ frontend/App.jsx
@@ -0,0 +1,25 @@
+﻿import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+import { FetchData } from './components/FetchData';
+import { Counter } from './components/Counter';
+import { Login } from './components/login';
+import { AccountHome } from './components/AccountHome';
+
+export default class App extends Component {
+    displayName = App.name
+
+    render() {
+        return (
+            <Layout>
+                <Route exact path='/' component={Home} />
+                <Route path='/counter' component={Counter} />
+                <Route path='/fetchdata' component={FetchData} />
+                <Route path='/login' component={Login} />
+                <Route path='/register' component={Register} />
+                <Route path='/accountHome' component={AccountHome} />
+            </Layout>
+        );
+    }
+}
