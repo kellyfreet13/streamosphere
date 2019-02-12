12389b10c392882aa17be85cb77dbad49a960b9d
Khoa100 <minhkhoavu954@gmail.com>
2019-02-12 13:28:37 -0800

    Update register.jsx

--- frontend/components/register.jsx
+++ frontend/components/register.jsx
@@ -1,13 +1,32 @@
-﻿import React, { Component } from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            pass: ''
+        };
+
+        this.GetInput = this.GetInput.bind(this);
+    }
+
+    GetInput(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
     render() {
         return (
             <div>
                 <form>
-                    <label> New username: <input type="text" /> <br />
-                        <label> New password: <input type="password" /> <br />
+                    <label> New username: <input type="text" name="username" /> <br />
+                        <label> New password: <input type="password" name="pass" /> <br />
                             <Link to="/AccountHome"><button type="Submit">Submit</button></Link>
                         </label>
                     </label>
