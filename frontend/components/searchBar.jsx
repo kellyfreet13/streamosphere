4c2940f337d27f752727e551b52abcadf69e0db4
Keatnisher <keat.nishimi@gmail.com>
2019-03-05 13:53:11 -0800

    Add files via upload

---
+++ frontend/components/searchBar.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import SearchBar from 'material-ui-search-bar';
+
+export class searchBar extends Component {
+    render() {
+        return (
+            <div>
+                <SearchBar
+                    value={this.state.value}
+                    onChange={(newValue) => this.setState({ value: newValue })}
+                    onRequestSearch={() => doSomethingWith(this.state.value)}
+                    style={{
+                        margin: '0 auto',
+                        maxWidth: 800
+                    }}
+                />
+            </div>
+        )
+    }
+}
