8a9b3bced4e93c9c929207e437d66c730746af57
kellyfreet13 <kelly.freet13@yahoo.com>
2019-04-18 01:05:27 -0700

    anthony's security

---
+++ backend/security/secure_requests.go
@@ -0,0 +1,28 @@
+package security
+
+import (
+	"crypto/tls"
+	"golang.org/x/crypto/acme/autocert"
+	"net/http"
+)
+
+// Adds TLS to the server by adding TLSConfig
+// @Params
+// server: server to accept traffic.
+// @Returns
+// certManager: Certification Manager to use when server is started.
+func SecureHTTPS(server *http.Server) (certManager autocert.Manager) {
+	// certManager gets the CA certificate and key for TLS handshake.
+	// TODO(anthonyasanchez): Add certs folder to a config.
+	certManager = autocert.Manager{
+		Prompt: autocert.AcceptTOS,
+		Cache:  autocert.DirCache("../certs"),
+	}
+
+	// Use the TSL config from autocert api.
+	server.TLSConfig = &tls.Config{
+		GetCertificate: certManager.GetCertificate,
+	}
+
+	return certManager
+}
