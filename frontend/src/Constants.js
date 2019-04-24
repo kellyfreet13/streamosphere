243f2943fd72361fd980ead37f91570258f95015
kellyfreet13 <kelly.freet13@yahoo.com>
2019-04-23 17:05:29 -0700

    adding some constants for s3

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,2 +1,5 @@
 export const HOME_URL = "http://streamosphere.net:8080";
 export const API_URL  = "http://streamosphere.net:8080/api";
+export const TEST_USER_ID = '5c9acddba0f0b4e94109c632';
+export const BUCKET_NAME = 'ec2-54-215-161-219.media';
+export const S3_REGION = 'us-west-1';
