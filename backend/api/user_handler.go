b62a9ec38e9e024dea6393c3d3367121875ccd48
Ubuntu <ubuntu@ip-172-31-13-191.us-west-1.compute.internal>
2019-04-25 05:59:13 +0000

    create user api call handled

--- backend/api/user_handler.go
+++ backend/api/user_handler.go
@@ -14,10 +14,10 @@ import (
 )
 
 type User struct {
-    ID          primitive.ObjectID  `bson:"_id,omitempty" json:"id"`
-    FirstName   string              `bson:"FirstName,omitempty"`
-    LastName    string              `bson:"LastName,omitempty"`
-    Email       string              `bson:"Email,omitempty"`
+    ID          primitive.ObjectID  `bson:"_id,omitempty"       json:"id,omitempty"`
+    FirstName   string              `bson:"FirstName,omitempty" json:"FirstName,omitempty"`
+    LastName    string              `bson:"LastName,omitempty"  json:"LastName,omitempty"`
+    Email       string              `bson:"Email,omitempty"     json:"Email,omitempty"`
 }
 
 // get all users
@@ -40,4 +40,24 @@ func GetUsers(c *gin.Context) {
 
     c.Header("Content-Type", "application/json")
     c.JSON(http.StatusOK, users)
+}
+
+// ex: { "Email": "kelly@sexy.gov" }
+func CreateUser(c *gin.Context) {
+    client := GetMongoClient()
+    user_coll := client.Database("streamosphere").Collection("users")
+
+    // bind json body to struct
+    var json_user User
+    c.BindJSON(&json_user)
+
+    // insert into db and get result
+    res, err := user_coll.InsertOne(context.TODO(), json_user)
+    if err != nil { log.Println(err); }
+
+    // return the id created from the db
+    c.Header("Content-Type", "application/json")
+    c.JSON(http.StatusOK, gin.H {
+        "UserID": res.InsertedID,
+    })
 }
